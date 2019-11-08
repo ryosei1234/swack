c4202e3a0c4aecd64276c4c3398a262ee3e8ae19
s20183044 <s20183044@HCS41228.stdlan.hcs.local>
2019-11-08 10:42:21 +0900

    ルーム作成サーブレット

---
+++ src/servlet/CreateRoomServlet.java
@@ -0,0 +1,32 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import dao.RoomDAO;
+
+/**
+ * Servlet implementation class CreateRoomServlet
+ */
+@WebServlet("/CreateRoomServlet")
+public class CreateRoomServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String roomname = request.getParameter("roomname");
+		String username = request.getParameter("username");
+
+		RoomDAO roomdao = new RoomDAO();
+		roomdao.saveRoom(username, roomname);
+		request.getRequestDispatcher("WEB-INF/index.jsp").forward(request, response);
+	}
+
+}
