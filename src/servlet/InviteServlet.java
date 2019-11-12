7dc1c51dde675ec65d33e3f5f3aeb1f9315355dc
s20183044 <s20183044@HCS41228.stdlan.hcs.local>
2019-11-12 11:49:01 +0900

    招待機能サーブレット

---
+++ src/servlet/InviteServlet.java
@@ -0,0 +1,40 @@
+package servlet;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import dao.UserDAO;
+
+/**
+ * Servlet implementation class InviteServlet
+ */
+@WebServlet("/InviteServlet")
+public class InviteServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String roomname = request.getParameter("roomname");
+		String username = request.getParameter("username");
+
+		if (username == null) {
+			UserDAO userDAO = new UserDAO();
+			ArrayList<String> userlist = userDAO.getUserList();
+			request.setAttribute("roomname", roomname);
+			request.setAttribute("userlist", userlist);
+			request.getRequestDispatcher("WEB-INF/Invite.jsp").forward(request, response);
+		} else {
+
+		}
+
+	}
+
+}
